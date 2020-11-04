c8893e4b7dac5d1249f230448443b8d3e3d77aef
“Carson <carson.mcmanus1@gmail.com>
2020-11-04 12:39:32 -0500

    record audio as int32 to save on disk space

--- recording.go
+++ recording.go
@@ -7,8 +7,7 @@ import (
 	"github.com/zimmski/osutil"
 )
 
-// TODO: record audio with type that makes sense, not the type that fits into the line chart the easiest
-var audioStream chan []float64 = make(chan []float64)
+var audioStream chan []int32 = make(chan []int32)
 
 func record() {
 	const bufSize = 1024
@@ -34,30 +33,29 @@ func record() {
 	}
 
 	log.Print("Recording started")
-	samples := make([]float64, bufSize)
 	for {
 		err := stream.Read()
 		if err != nil {
 			log.Fatalf("Failed to read stream audio: %s", err)
 		}
-
-		for i, s := range in {
-			samples[i] = float64(s)
-		}
-
-		audioStream <- samples
+		audioStream <- in
 	}
 }
 
-var recordedAudio []float64 = make([]float64, 0, 44100)
+var recordedAudio []int32 = make([]int32, 0, 44100)
 
 func audioProcessor() {
 	log.Print("Audio processing started")
+	const displayBufferSize = 44100
 	for {
 		buffer := <-audioStream
 		recordedAudio = append(recordedAudio, buffer...)
 
-		displayBuffer := recordedAudio[clamp(len(recordedAudio)-44100, 0, len(recordedAudio)):]
+		dispOffset := clamp(len(recordedAudio)-displayBufferSize, 0, len(recordedAudio))
+		displayBuffer := make([]float64, displayBufferSize)
+		for i, s := range recordedAudio[dispOffset:] {
+			displayBuffer[i] = float64(s)
+		}
 		waveformWidget.Series("Waveform", displayBuffer)
 	}
 }
