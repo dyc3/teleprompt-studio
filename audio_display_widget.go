bcfdc42f2888b44c08c8f934804ca3f7067676d0
“Carson <carson.mcmanus1@gmail.com>
2020-11-05 21:30:03 -0500

    add scrolling to zoom waveform

--- audio_display_widget.go
+++ audio_display_widget.go
@@ -160,6 +160,10 @@ func (w *AudioDisplayWidget) Mouse(m *terminalapi.Mouse) error {
 			log.Printf("drag select start %s", w.selected.Start)
 			w.lastClickStart = m.Position
 		}
+	} else if m.Button == mouse.ButtonWheelDown {
+		w.window.Start -= w.window.Duration() / 10
+	} else if m.Button == mouse.ButtonWheelUp {
+		w.window.Start += w.window.Duration() / 10
 	}
 
 	if w.selectionActive && m.Button == mouse.ButtonRelease {
