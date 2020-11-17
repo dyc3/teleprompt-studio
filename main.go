0b12c68e66bce966076a356b78c422206c02e759
Carson McManus <carson.mcmanus1@gmail.com>
2020-11-17 17:08:08 -0500

    refactor keybinds

--- main.go
+++ main.go
@@ -41,8 +41,9 @@ type widgets struct {
 var ui widgets
 
 type keybind struct {
-	key  keyboard.Key
-	desc string
+	key      keyboard.Key
+	desc     string
+	callback func()
 }
 
 func getAvailableKeybinds() []keybind {
@@ -50,59 +51,75 @@ func getAvailableKeybinds() []keybind {
 	if !isRecordingTake {
 		keys = append(keys, []keybind{
 			{
-				key:  keyboard.KeyArrowDown,
-				desc: "Next Chunk",
+				key:      keyboard.KeyArrowDown,
+				desc:     "Next Chunk",
+				callback: keybindNextChunk,
 			},
 			{
-				key:  keyboard.KeyArrowUp,
-				desc: "Previous Chunk",
+				key:      keyboard.KeyArrowUp,
+				desc:     "Previous Chunk",
+				callback: keybindPreviousChunk,
 			},
 			{
-				key:  ' ',
-				desc: "Start Take",
+				key:      ' ',
+				desc:     "Start Take",
+				callback: func() { startTake() },
 			},
 			{
-				key:  'g',
-				desc: "Mark Good",
+				key:      'g',
+				desc:     "Mark Good",
+				callback: keybindMarkGood,
 			},
 			{
-				key:  'b',
-				desc: "Mark Bad",
+				key:      'b',
+				desc:     "Mark Bad",
+				callback: keybindMarkBad,
 			},
 			{
-				key:  'p',
-				desc: "Play Selected Take",
+				key:      'p',
+				desc:     "Play Selected Take",
+				callback: keybindPlayTake,
 			},
 		}...)
 
 		if ui.audio.selectionActive {
 			keys = append(keys,
 				keybind{
-					key:  't',
-					desc: "New Take from selection",
+					key:      't',
+					desc:     "New Take from selection",
+					callback: keybindCreateTakeFromSelection,
 				},
 			)
 		}
 	} else {
 		keys = append(keys, []keybind{
 			{
-				key:  ' ',
-				desc: "End Take",
+				key:      ' ',
+				desc:     "End Take",
+				callback: func() { endTake() },
 			},
 			{
-				key:  'g',
-				desc: "End Take & Mark Good",
+				key:      'g',
+				desc:     "End Take & Mark Good",
+				callback: keybindMarkGood,
 			},
 			{
-				key:  'b',
-				desc: "End Take & Mark Bad",
+				key:      'b',
+				desc:     "End Take & Mark Bad",
+				callback: keybindMarkBad,
 			},
 		}...)
 	}
 	keys = append(keys, []keybind{
 		{
-			key:  'f',
-			desc: "Toggle Stick Viewport To End",
+			key:      'f',
+			desc:     "Toggle Stick Viewport To End",
+			callback: func() { ui.audio.stickToEnd = !ui.audio.stickToEnd },
+		},
+		{
+			key:      keyboard.KeyCtrlD,
+			desc:     "Toggle Debug",
+			callback: func() { ui.audio.showDebug = !ui.audio.showDebug },
 		},
 	}...)
 	return keys
@@ -204,69 +221,78 @@ func updateControlsDisplay() {
 	}
 }
 
+func keybindPreviousChunk() {
+	if isRecordingTake {
+		return
+	}
+	if selectedChunk > 0 {
+		selectedChunk -= 1
+	}
+	chunk := currentSession.Doc.GetChunk(int(selectedChunk))
+	selectedTake = len(chunk.Takes) - 1
+}
+
+func keybindNextChunk() {
+	if isRecordingTake {
+		return
+	}
+	if selectedChunk < uint(currentSession.Doc.CountChunks()-1) {
+		selectedChunk += 1
+	}
+	chunk := currentSession.Doc.GetChunk(int(selectedChunk))
+	selectedTake = len(chunk.Takes) - 1
+}
+
+func keybindMarkGood() {
+	currentSession.Doc.GetChunk(int(selectedChunk)).Takes[selectedTake].Mark = Good
+	if isRecordingTake {
+		endTake()
+	}
+}
+
+func keybindMarkBad() {
+	currentSession.Doc.GetChunk(int(selectedChunk)).Takes[selectedTake].Mark = Bad
+	if isRecordingTake {
+		endTake()
+	}
+}
+
+func keybindCreateTakeFromSelection() {
+	if ui.audio.selectionActive {
+		chunk := currentSession.Doc.GetChunk(int(selectedChunk))
+		take := Take{}
+		take.Start = ui.audio.selected.Start
+		take.End = ui.audio.selected.End
+		chunk.Takes = append(chunk.Takes, take)
+		selectedTake = len(chunk.Takes) - 1
+		ui.audio.Deselect()
+	}
+}
+
+func keybindEndSession() {
+	err := EndSession()
+	if err != nil {
+		log.Print(err)
+	}
+}
+
+func keybindPlayTake() {
+	take := currentSession.Doc.GetChunk(int(selectedChunk)).Takes[selectedTake]
+	go playbackTake(take)
+}
+
 func globalKeyboardHandler(k *terminalapi.Keyboard) {
 	if k.Key == keyboard.KeyEsc || k.Key == keyboard.KeyCtrlC {
 		portaudio.Terminate()
 		terminal.Close()
 		cancelGlobal()
-	} else if k.Key == keyboard.KeyArrowUp {
-		if isRecordingTake {
-			return
-		}
-		if selectedChunk > 0 {
-			selectedChunk -= 1
-		}
-		chunk := currentSession.Doc.GetChunk(int(selectedChunk))
-		selectedTake = len(chunk.Takes) - 1
-	} else if k.Key == keyboard.KeyArrowDown {
-		if isRecordingTake {
-			return
-		}
-		if selectedChunk < uint(currentSession.Doc.CountChunks()-1) {
-			selectedChunk += 1
-		}
-		chunk := currentSession.Doc.GetChunk(int(selectedChunk))
-		selectedTake = len(chunk.Takes) - 1
-	} else if k.Key == ' ' {
-		if !isRecordingTake {
-			startTake()
-		} else {
-			endTake()
-		}
-	} else if k.Key == 'g' {
-		currentSession.Doc.GetChunk(int(selectedChunk)).Takes[selectedTake].Mark = Good
-		if isRecordingTake {
-			endTake()
-		}
-	} else if k.Key == 'b' {
-		currentSession.Doc.GetChunk(int(selectedChunk)).Takes[selectedTake].Mark = Bad
-		if isRecordingTake {
-			endTake()
-		}
-	} else if k.Key == 't' {
-		if ui.audio.selectionActive {
-			chunk := currentSession.Doc.GetChunk(int(selectedChunk))
-			take := Take{}
-			take.Start = ui.audio.selected.Start
-			take.End = ui.audio.selected.End
-			chunk.Takes = append(chunk.Takes, take)
-			selectedTake = len(chunk.Takes) - 1
-			ui.audio.Deselect()
-		}
-	} else if k.Key == 'r' {
-		err := EndSession()
-		if err != nil {
-			log.Print(err)
-		}
-	} else if k.Key == 'p' {
-		take := currentSession.Doc.GetChunk(int(selectedChunk)).Takes[selectedTake]
-		go playbackTake(take)
-	} else if k.Key == 'f' {
-		ui.audio.stickToEnd = !ui.audio.stickToEnd
-	} else if k.Key == keyboard.KeyCtrlD {
-		ui.audio.showDebug = !ui.audio.showDebug
 	} else {
-		log.Printf("Unknown key pressed: %v", k)
+		for _, bind := range getAvailableKeybinds() {
+			if bind.key == k.Key {
+				bind.callback()
+				break
+			}
+		}
 	}
 
 	updateControlsDisplay()
